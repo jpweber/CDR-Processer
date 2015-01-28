4b4e1368cb288eb346441de84e957db1c2e54629
James Weber <jpweber@gmail.com>
2015-01-28 16:48:18 -0500

    initial commit with a basic parsing of CSV file and creating a map from it with named values

---
+++ Load.go
@@ -0,0 +1,68 @@
+/*
+* @Author: Jim Weber
+* @Date:   2015-01-28 11:48:33
+* @Last Modified by:   Jim Weber
+* @Last Modified time: 2015-01-28 16:45:30
+ */
+
+package main
+
+import (
+	"encoding/csv"
+	// "encoding/json"
+	"fmt"
+	"os"
+	// "strconv"
+	"bluetone/cdrtest/CDRlib"
+	"reflect"
+)
+
+func main() {
+	csvFile, err := os.Open("./data.csv")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer csvFile.Close()
+
+	reader := csv.NewReader(csvFile)
+
+	reader.FieldsPerRecord = -1
+
+	csvData, err := reader.ReadAll()
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// fmt.Println(csvData)
+
+	// for _, value := range csvData[0] {
+	// 	fmt.Println(value)
+	// }
+
+	cdrKeys := make([]string, 0)
+	stopCDR := new(CDR.StopCDR)
+	s := reflect.ValueOf(stopCDR).Elem()
+	typeOfT := s.Type()
+	for i := 0; i < s.NumField(); i++ {
+		// f := s.Field(i)
+		// fmt.Printf("%d: %s \n", i, typeOfT.Field(i).Name)
+		cdrKeys = append(cdrKeys, typeOfT.Field(i).Name)
+	}
+
+	// fmt.Println(cdrKeys)
+	cdrData := make(map[string]string)
+	for _, value := range csvData {
+		// fmt.Println(i)
+		// fmt.Println(value[2])
+		// fmt.Println(reflect.ValueOf(value))
+		cdrData = CDR.FillCDRMap(cdrKeys, value)
+	}
+
+	// fmt.Println(cdrData)
+	fmt.Println(cdrData["Ingress_Protocol_Variant_Spec_Data"])
+
+}
