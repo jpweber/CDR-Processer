bf74ccab34888c28b064c5dd4b01c65c2f5aa56b
James Weber <jpweber@gmail.com>
2015-01-29 11:26:33 -0500

    Changed from using a CDR struct to having an array of key names and building a map instead

--- Load.go
+++ Load.go
@@ -1,8 +1,8 @@
 /*
 * @Author: Jim Weber
 * @Date:   2015-01-28 11:48:33
-* @Last Modified by:   Jim Weber
-* @Last Modified time: 2015-01-28 16:45:30
+* @Last Modified by:   jpweber
+* @Last Modified time: 2015-01-29 11:26:00
  */
 
 package main
@@ -14,11 +14,11 @@ import (
 	"os"
 	// "strconv"
 	"bluetone/cdrtest/CDRlib"
-	"reflect"
+	// "reflect"
 )
 
 func main() {
-	csvFile, err := os.Open("./data.csv")
+	csvFile, err := os.Open("./1000309.ACT")
 
 	if err != nil {
 		fmt.Println(err)
@@ -43,26 +43,30 @@ func main() {
 	// 	fmt.Println(value)
 	// }
 
-	cdrKeys := make([]string, 0)
-	stopCDR := new(CDR.StopCDR)
-	s := reflect.ValueOf(stopCDR).Elem()
-	typeOfT := s.Type()
-	for i := 0; i < s.NumField(); i++ {
-		// f := s.Field(i)
-		// fmt.Printf("%d: %s \n", i, typeOfT.Field(i).Name)
-		cdrKeys = append(cdrKeys, typeOfT.Field(i).Name)
-	}
+	cdrKeys := CDR.CdrStopKeys()
+	// cdrKeys := make([]string, 0)
+	// stopCDR := new(CDR.StopCDR)
+	// s := reflect.ValueOf(stopCDR).Elem()
+	// typeOfT := s.Type()
+	// for i := 0; i < s.NumField(); i++ {
+	// 	// f := s.Field(i)
+	// 	// fmt.Printf("%d: %s \n", i, typeOfT.Field(i).Name)
+	// 	cdrKeys = append(cdrKeys, typeOfT.Field(i).Name)
+	// }
 
 	// fmt.Println(cdrKeys)
+	records := make([]map[string]string, 0)
 	cdrData := make(map[string]string)
 	for _, value := range csvData {
 		// fmt.Println(i)
 		// fmt.Println(value[2])
 		// fmt.Println(reflect.ValueOf(value))
 		cdrData = CDR.FillCDRMap(cdrKeys, value)
+		records = append(records, cdrData)
 	}
 
 	// fmt.Println(cdrData)
-	fmt.Println(cdrData["Ingress_Protocol_Variant_Spec_Data"])
+	// fmt.Println(records)
+	fmt.Println("Done parsing file")
 
 }
