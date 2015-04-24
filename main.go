5ec67b050e0c5d7a8d782c8ff0524fe634f53e2e
James Weber <jpweber@gmail.com>
2015-04-23 21:20:03 -0400

    Main will now stay running if there are no files to process it sleeps for 60 seconds.
    This time is going to get moved out to config file

--- main.go
+++ main.go
@@ -2,7 +2,7 @@
 * @Author: Jim Weber
 * @Date:   2015-01-28 11:48:33
 * @Last Modified by:   jpweber
-* @Last Modified time: 2015-04-23 16:00:26
+* @Last Modified time: 2015-04-23 21:13:24
  */
 
 //parses CDR file in to key value map and then does something with it
@@ -18,11 +18,12 @@ import (
 	"flag"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
-	"io/ioutil"
 	"ko/CDR"
+	"ko/FileHandling"
 	"os"
 	"strings"
 	"sync"
+	"time"
 )
 
 type Configuration struct {
@@ -116,22 +117,6 @@ func saveRecord(wg *sync.WaitGroup, db sql.DB, records []map[string]string, reco
 	// wg.Done()
 }
 
-func fileList(dir string) []string {
-	files, err := ioutil.ReadDir(dir)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	fileNames := make([]string, 0)
-
-	for _, file := range files {
-		if !file.IsDir() {
-			fileNames = append(fileNames, file.Name())
-		}
-	}
-	return fileNames
-}
-
 func main() {
 
 	var buildNumber string
@@ -156,83 +141,97 @@ func main() {
 		fmt.Println("error:", err)
 	}
 
-	// var fileToOpen string
-	var files []string
-	if *cdrFileName != "" {
-		fileToOpen := *cdrFileName
-		files = append(files, fileToOpen)
-	} else {
-		files = fileList(configuration.FileDir)
-	}
+	for {
+		// var fileToOpen string
+		var files []string
+		if *cdrFileName != "" {
+			fileToOpen := *cdrFileName
+			files = append(files, fileToOpen)
+		} else {
+			files = FileHandling.FileList(configuration.FileDir)
+		}
 
-	// start db stuff
-	db, err := sql.Open("mysql", configuration.DSN)
-	if err != nil {
-		// log.Fatal(err)
-		fmt.Println(err)
-	}
+		if len(files) == 0 {
+			fmt.Println("Not files waiting. Sleeping for 60 seconds")
+			time.Sleep(time.Second * 60)
+			continue
+		}
 
-	//test our connection to make sure the DB is reachable.
-	err = db.Ping()
-	if err != nil {
-		fmt.Println(err)
-	}
+		// start db stuff
+		db, err := sql.Open("mysql", configuration.DSN)
+		if err != nil {
+			// log.Fatal(err)
+			fmt.Println(err)
+		}
 
-	defer db.Close()
-
-	// I don't like this giant loop but its a simple way to start and test
-	for _, file := range files {
-		csvFile, err := os.Open("ACT/" + file)
+		//test our connection to make sure the DB is reachable.
+		err = db.Ping()
 		if err != nil {
 			fmt.Println(err)
 		}
 
-		reader := csv.NewReader(csvFile)
-		reader.FieldsPerRecord = -1
-		csvData, err := reader.ReadAll()
+		defer db.Close()
 
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
+		// I don't like this giant loop but its a simple way to start and test
+		for _, file := range files {
+			csvFile, err := os.Open("ACT/" + file)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			reader := csv.NewReader(csvFile)
+			reader.FieldsPerRecord = -1
+			csvData, err := reader.ReadAll()
+
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			csvFile.Close()
+
+			cdrCollection := CDR.SplitTypes(csvData)
+			if cdrCollection != nil {
+				//if we've created a cdr collection we no longer need csvData
+				//nil it out to release memory for GC
+				csvData = nil
+			}
+
+			var wg sync.WaitGroup
+			wg.Add(3) //We need to wait for Stops, attempts and starts to finish on this wait group
+			//If more go funcs are added the integer here needs increased to match
+
+			//setup our containers for CDR data
+			stopRecords := make([]map[string]string, len(cdrCollection.Stops))
+			attemptRecords := make([]map[string]string, len(cdrCollection.Attempts))
+			startRecords := make([]map[string]string, len(cdrCollection.Starts))
+
+			//Populate the containers of CDR Data
+			go func() {
+				stopRecords = (CDR.CreateRecordMap(&wg, cdrCollection.Stops, "stops"))
+			}()
+			go func() {
+				attemptRecords = (CDR.CreateRecordMap(&wg, cdrCollection.Attempts, "attempts"))
+			}()
+			go func() {
+				startRecords = (CDR.CreateRecordMap(&wg, cdrCollection.Starts, "starts"))
+			}()
+
+			wg.Wait() //Wait for the concurrent routines to call 'done'
+			fmt.Println("Done parsing file")
+			res := FileHandling.ArchiveFile(file)
+			if res != true {
+				fmt.Println("Error moving file")
+				fmt.Println(file)
+				os.Exit(1)
+			}
+
+			//Begin inserting CDR Data
+			wg.Add(3)
+			go saveRecord(&wg, *db, stopRecords, "stops", transactionChunk)
+			go saveRecord(&wg, *db, attemptRecords, "attempts", transactionChunk)
+			go saveRecord(&wg, *db, startRecords, "starts", transactionChunk)
+			wg.Wait() //Wait for the concurrent routines to call 'done'
 		}
-		csvFile.Close()
-
-		cdrCollection := CDR.SplitTypes(csvData)
-		if cdrCollection != nil {
-			//if we've created a cdr collection we no longer need csvData
-			//nil it out to release memory for GC
-			csvData = nil
-		}
-
-		var wg sync.WaitGroup
-		wg.Add(3) //We need to wait for Stops, attempts and starts to finish on this wait group
-		//If more go funcs are added the integer here needs increased to match
-
-		//setup our containers for CDR data
-		stopRecords := make([]map[string]string, len(cdrCollection.Stops))
-		attemptRecords := make([]map[string]string, len(cdrCollection.Attempts))
-		startRecords := make([]map[string]string, len(cdrCollection.Starts))
-
-		//Populate the containers of CDR Data
-		go func() {
-			stopRecords = (CDR.CreateRecordMap(&wg, cdrCollection.Stops, "stops"))
-		}()
-		go func() {
-			attemptRecords = (CDR.CreateRecordMap(&wg, cdrCollection.Attempts, "attempts"))
-		}()
-		go func() {
-			startRecords = (CDR.CreateRecordMap(&wg, cdrCollection.Starts, "starts"))
-		}()
-
-		wg.Wait() //Wait for the concurrent routines to call 'done'
-		fmt.Println("Done parsing file")
-
-		//Begin inserting CDR Data
-		wg.Add(3)
-		go saveRecord(&wg, *db, stopRecords, "stops", transactionChunk)
-		go saveRecord(&wg, *db, attemptRecords, "attempts", transactionChunk)
-		go saveRecord(&wg, *db, startRecords, "starts", transactionChunk)
-		wg.Wait() //Wait for the concurrent routines to call 'done'
 	}
 
 }
